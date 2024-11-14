fece5d659636e42d81b6ac22204b03217a612be6
Dmitry <mistapecss@yandex.ru>
2024-11-14 10:30:21 +0300

    KTB-02-create word wile

--- src/main/kotlin/create_file.kt
+++ src/main/kotlin/create_file.kt
@@ -4,9 +4,6 @@ import java.io.File
 
 fun main() {
     val wordFile: File = File("words.txt")
-    wordFile.delete()
-    wordFile.appendText("hello привет\ndog собака\ncat кошка")
 
-    //wordFile.readLines().forEach{ println(it) }
-    for (i in wordFile.readLines()) println(i)
+    wordFile.forEachLine { println(it) }
 }
