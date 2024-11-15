0e9143ea2d7b747bf87526ba68599d571e6da1bc
Dmitry <mistapecss@yandex.ru>
2024-11-15 11:57:12 +0300

    KTB-04-create dictionary

--- src/main/kotlin/create_file.kt
+++ src/main/kotlin/create_file.kt
@@ -2,8 +2,22 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswerCount: Int? = 0,
+)
+
 fun main() {
     val wordFile: File = File("words.txt")
 
-    wordFile.forEachLine { println(it) }
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    wordFile.forEachLine {
+        val lines = it.split("|", "+");
+        val word: Word =
+            Word(original = lines[0], translate = lines[1], correctAnswerCount = lines[2].toIntOrNull()?:0);
+        dictionary.add(word)
+    }
+println(dictionary)
 }
