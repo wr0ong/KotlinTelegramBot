193d62df2128c1821e887df83a690c64eb52011d
wr0ong <mistapecss@yandex.ru>
2024-11-20 16:22:18 +0300

    KTB-06 create state-info block

--- src/main/kotlin/create_file.kt
+++ src/main/kotlin/create_file.kt
@@ -25,29 +25,28 @@ fun loadDictionary(wordFile: File): List<Word> {
 
 fun main() {
     val wordFile: File = File("words.txt")
-    val realizationLoadDictionary = loadDictionary(wordFile)
-    println(realizationLoadDictionary)
-    val listOfAnswerLearned: MutableList<Word> = mutableListOf()
+    val dictionary = loadDictionary(wordFile)
+    println(dictionary)
 
     while (true) {
         println("Меню:\n1 - Учить слова\n2 - Статистика\n0 - Выход")
-        val choice: Int = readln().toInt()
-        when (choice) {
-            1 -> println("Выбран пункт \"Учить слова\"")
-            2 -> {
-                realizationLoadDictionary.filter { it.correctAnswerCount >= 3 }
-                    .forEach {
-                        if (it !in listOfAnswerLearned)
-                        listOfAnswerLearned.add(it)
-                    }
-                val learnedCount = listOfAnswerLearned.size
-                val totalCount = realizationLoadDictionary.size
-                val percent = learnedCount.toDouble() / totalCount.toDouble() * 100
-                println(String.format("Выучено %d из %d слов | %.0f%%\n", learnedCount, totalCount, percent))
-            }
+        try {
+            val choice: Int = readln().toInt()
+            when (choice) {
+                1 -> println("Выбран пункт \"Учить слова\"")
+                2 -> {
+                    val learnedCount = dictionary.filter { it.correctAnswerCount >= 3 }
+                        .size
+                    val totalCount = dictionary.size
+                    val percent = learnedCount.toDouble() / totalCount.toDouble() * 100
+                    println(String.format("Выучено %d из %d слов | %.0f%%\n", learnedCount, totalCount, percent))
+                }
 
-            0 -> break
-            else -> println("Введите число 0, 1 или 2")
+                0 -> break
+                else -> println("Введите число 0, 1 или 2")
+            }
+        } catch (e: Exception) {
+            println("Введите число 0, 1 или 2")
         }
     }
 
