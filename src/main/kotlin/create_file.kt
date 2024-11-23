d149ecacfae944a498f75e4a06894ab6f588015e
wr0ong <mistapecss@yandex.ru>
2024-11-23 12:46:58 +0300

    KTB-08 create ready-question-block

--- src/main/kotlin/create_file.kt
+++ src/main/kotlin/create_file.kt
@@ -40,6 +40,7 @@ fun main() {
             val choice: Int = readln().toInt()
             when (choice) {
                 1 -> {
+
                     while (true) {
                         var choiceOfUser: Int
                         val notLearnedList = dictionary.filter { it.correctAnswerCount < CORRECT_ANSWERS_TO_LEARN }
@@ -54,55 +55,45 @@ fun main() {
                                 if (choiceOfUser !in 0..ANSWER_OPTIONS) {
                                     println("Выберите ответ 1, 2, 3 или 4")
                                 } else {
+
                                     when (choiceOfUser) {
                                         0 -> break
-                                        1 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
-                                                questionWords[0]
-                                            )
-                                        ) {
+                                        1 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(questionWords[0])) {
                                             println("Верно!")
                                             questionWords[0].correctAnswerCount++
                                             saveDictionary(dictionary)
                                         } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
 
-                                        2 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
-                                                questionWords[1]
-                                            )
-                                        ) {
+                                        2 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(questionWords[1])) {
                                             println("Верно!")
                                             questionWords[1].correctAnswerCount++
                                             saveDictionary(dictionary)
                                         } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
 
-                                        3 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
-                                                questionWords[2]
-                                            )
-                                        ) {
+                                        3 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(questionWords[2])) {
                                             println("Верно!")
                                             questionWords[2].correctAnswerCount++
                                             saveDictionary(dictionary)
                                         } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
 
-                                        4 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
-                                                questionWords[3]
-                                            )
-                                        ) {
+                                        4 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(questionWords[3])) {
                                             println("Верно!")
                                             questionWords[3].correctAnswerCount++
                                             saveDictionary(dictionary)
                                         } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
                                     }
+
                                 }
                             } catch (e: Exception) {
                                 println("Выберите ответ 1, 2, 3 или 4")
                             }
                         }
                     }
+
                 }
 
                 2 -> {
-                    val learnedCount = dictionary.filter { it.correctAnswerCount >= CORRECT_ANSWERS_TO_LEARN }
-                        .size
+                    val learnedCount = dictionary.filter { it.correctAnswerCount >= CORRECT_ANSWERS_TO_LEARN }.size
                     val totalCount = dictionary.size
                     val percent = learnedCount.toDouble() / totalCount.toDouble() * 100
                     println(String.format("Выучено %d из %d слов | %.0f%%\n", learnedCount, totalCount, percent))
