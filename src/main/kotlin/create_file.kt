39bc233f402f797b811c37d8743268f9aae1632f
wr0ong <mistapecss@yandex.ru>
2024-11-23 12:44:19 +0300

    KTB-08 create ready-question-block

--- src/main/kotlin/create_file.kt
+++ src/main/kotlin/create_file.kt
@@ -5,7 +5,7 @@ import java.io.File
 data class Word(
     val original: String,
     val translate: String,
-    val correctAnswerCount: Int = 0,
+    var correctAnswerCount: Int = 0,
 )
 
 fun loadDictionary(wordFile: File): List<Word> {
@@ -28,14 +28,80 @@ fun main() {
     val dictionary = loadDictionary(wordFile)
     println(dictionary)
 
+    fun saveDictionary(dictionary: List<Word>) {
+        var newText = dictionary.joinToString {"${it.original}|${it.translate}|${it.correctAnswerCount}"}
+        newText = newText.replace(", ", "\n")
+        wordFile.writeText(newText)
+    }
+
     while (true) {
         println("Меню:\n1 - Учить слова\n2 - Статистика\n0 - Выход")
         try {
             val choice: Int = readln().toInt()
             when (choice) {
-                1 -> println("Выбран пункт \"Учить слова\"")
+                1 -> {
+                    while (true) {
+                        var choiceOfUser: Int
+                        val notLearnedList = dictionary.filter { it.correctAnswerCount < CORRECT_ANSWERS_TO_LEARN }
+                        if (notLearnedList.isEmpty()) println("Все слова в словаре выучены!")
+                        else {
+                            val questionWords = notLearnedList.shuffled().take(ANSWER_OPTIONS)
+                            val originalWord = questionWords.random()
+                            println(questionWords.mapIndexed { index, i -> "${index + 1} - ${i.translate}" }
+                                .joinToString("\n", "${originalWord.original}: \n", "\n----------- \n0 - Меню"))
+                            try {
+                                choiceOfUser = readln().toInt()
+                                if (choiceOfUser !in 0..ANSWER_OPTIONS) {
+                                    println("Выберите ответ 1, 2, 3 или 4")
+                                } else {
+                                    when (choiceOfUser) {
+                                        0 -> break
+                                        1 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
+                                                questionWords[0]
+                                            )
+                                        ) {
+                                            println("Верно!")
+                                            questionWords[0].correctAnswerCount++
+                                            saveDictionary(dictionary)
+                                        } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
+
+                                        2 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
+                                                questionWords[1]
+                                            )
+                                        ) {
+                                            println("Верно!")
+                                            questionWords[1].correctAnswerCount++
+                                            saveDictionary(dictionary)
+                                        } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
+
+                                        3 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
+                                                questionWords[2]
+                                            )
+                                        ) {
+                                            println("Верно!")
+                                            questionWords[2].correctAnswerCount++
+                                            saveDictionary(dictionary)
+                                        } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
+
+                                        4 -> if (questionWords.indexOf(originalWord) == questionWords.indexOf(
+                                                questionWords[3]
+                                            )
+                                        ) {
+                                            println("Верно!")
+                                            questionWords[3].correctAnswerCount++
+                                            saveDictionary(dictionary)
+                                        } else println("Неверно! ${originalWord.original} - это ${originalWord.translate}")
+                                    }
+                                }
+                            } catch (e: Exception) {
+                                println("Выберите ответ 1, 2, 3 или 4")
+                            }
+                        }
+                    }
+                }
+
                 2 -> {
-                    val learnedCount = dictionary.filter { it.correctAnswerCount >= 3 }
+                    val learnedCount = dictionary.filter { it.correctAnswerCount >= CORRECT_ANSWERS_TO_LEARN }
                         .size
                     val totalCount = dictionary.size
                     val percent = learnedCount.toDouble() / totalCount.toDouble() * 100
@@ -51,3 +117,6 @@ fun main() {
     }
 
 }
+
+const val ANSWER_OPTIONS = 4
+const val CORRECT_ANSWERS_TO_LEARN = 3
