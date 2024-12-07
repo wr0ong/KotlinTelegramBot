defab5400fb4ac39a16236c71a36e5c622acf8bd
wr0ong <mistapecss@yandex.ru>
2024-12-07 12:45:11 +0300

    KTB-16 create menu

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -1,7 +1,9 @@
 import java.net.URI
+import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
 
 class TelegramBotService(private val botToken: String) {
 
@@ -9,19 +11,51 @@ class TelegramBotService(private val botToken: String) {
 
     fun getUpdates(updateId: Int): String {
         val urlGetUpdates = "$LINK_TO_TG_API_BOT$botToken/getUpdates?offset=$updateId"
-
         val request = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-
         return response.body()
     }
 
     fun sendMessage(chatId: Int, text: String): String {
-
-        val urlGetMessage = "$LINK_TO_TG_API_BOT$botToken/sendMessage?chat_id=$chatId&text=$text"
+        val encoded = URLEncoder.encode(
+            text,
+            StandardCharsets.UTF_8
+        )
+        println(encoded)
+        val urlGetMessage = "$LINK_TO_TG_API_BOT$botToken/sendMessage?chat_id=$chatId&text=$encoded"
         val request = HttpRequest.newBuilder().uri(URI.create(urlGetMessage)).build()
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        return response.body()
+    }
 
+    fun sendMenu(chatId: Int): String {
+        val sendMessage = "$LINK_TO_TG_API_BOT$botToken/sendMessage"
+        val sendMenuBody = """
+            {
+                "chat_id": $chatId,
+                "text": "Основное меню",
+                "reply_markup": {
+                    "inline_keyboard": [
+                        [
+                            {
+                                "text": "Изучить слова",
+                                "callback_data": "learn_words_clicked"
+                            },
+                            {
+                                "text": "Статистика",
+                                "callback_data": "statistics_clicked"
+                            }
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+
+        val request = HttpRequest.newBuilder().uri(URI.create(sendMessage))
+            .header("Content-type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(sendMenuBody))
+            .build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
         return response.body()
     }
 }
