c6c6ec02c3976f63da3c904eaa8d00bbe8d8b7e7
wr0ong <mistapecss@yandex.ru>
2024-12-17 11:31:17 +0300

    KTB-21 create migration to serialize

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,7 +1,5 @@
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 @Serializable
 data class Update(
