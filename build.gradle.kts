0b9d927ca790f057129c5b13d898042a62b69b17
wr0ong <mistapecss@yandex.ru>
2024-12-18 13:38:56 +0300

    KTB-23 create readme.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.0.10"
+    kotlin("jvm") version "1.7.0"
     kotlin("plugin.serialization") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
 }
 
 group = "org.example"
@@ -17,4 +19,8 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("TelegramKt")
 }
